30822b8ff54b64a56cf761621c260139ed384691
mamamia140o <m-h-k140@hotmail.com>
2025-03-16 13:41:21 +0300

    refactored the code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,28 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float speed = 5f;
-    public float threshold = 1f;
     public float rotationSpeed = 500f;
     public float animationDampTime = 0.2f;
-    public float verticalSpeed = 0f;
-    public bool isGrounded = true;
+
+    [Header("Ground Check Settings")]
     public Vector3 groundCheckOffset;
-    public float groundCheckRadius;
-    public List<LayerMask> groundLayer;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private float verticalSpeed = 0f;
+    private Vector3 moveDirection = Vector3.zero;
     private Quaternion targetRotation;
     private Camera mainCamera;
     private Animator animator;
     private CharacterController characterController;
+
+    private bool IsGrounded
+    {
+        get => Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
 
     void Awake()
     {
@@ -27,47 +33,49 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+        ApplyGravity();
+        MovePlayer();
+    }
+
+    private void HandleMovement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        var moveInput = new Vector3(horizontal, 0, vertical).normalized;
-
-        var moveDir = mainCamera.transform.rotation * moveInput;
-
-        checkIsGrounded();
-        if (isGrounded)
+        Vector3 inputDirection = new Vector3(horizontal, 0, vertical).normalized;
+        if (inputDirection.magnitude > 0)
         {
-            verticalSpeed -= 0.5f;
+            moveDirection = mainCamera.transform.rotation * inputDirection;
+            targetRotation = Quaternion.LookRotation(moveDirection);
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+
+        animator.SetFloat("moveAmount", moveDirection.magnitude, animationDampTime, Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded)
+        {
+            verticalSpeed = -0.5f; // Small downward force to keep grounded
         }
         else
         {
             verticalSpeed += Physics.gravity.y * Time.deltaTime;
         }
+    }
 
-        var velocity = moveDir * speed;
+    private void MovePlayer()
+    {
+        Vector3 velocity = moveDirection * speed;
         velocity.y = verticalSpeed;
 
         characterController.Move(velocity * Time.deltaTime);
-
-        if (moveDir.magnitude > 0)
-        {
-            targetRotation = Quaternion.LookRotation(moveDir);
-        }
-
-        animator.SetFloat("moveAmount", Mathf.Clamp01(moveDir.magnitude), animationDampTime, Time.deltaTime);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-    }
-
-    private void checkIsGrounded()
-    {
-        foreach (var layer in groundLayer)
-        {
-            if (Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, layer))
-            {
-                isGrounded = true;
-            }
-        }
-        isGrounded = false;
     }
 
     private void OnDrawGizmosSelected()
@@ -75,5 +83,4 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
-
 }
